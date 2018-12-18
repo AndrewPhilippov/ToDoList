700ca3ceeea69139702d8407a6c4c1990c8619c8
Andrew <a@fandb.pro>
2018-12-18 18:55:20 +0300

    Small changes on functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,46 @@
+// Focus on input!!!
+$('input[type="text"]').css('display', 'none');
+
 // Line through on click event
-$('.todo__list-item-text').click(function(){
+$('ul').on('click', '.todo__list-item-text', function(){
   $(this).toggleClass('completed');
 });
 
 // Line through on click with check icon
-$('.check-item').click(function(){
+$('ul').on('click', '.check-item', function(){
   $(this).parent().siblings('.todo__list-item-text').toggleClass('completed');
 });
 
 // Delete Item
-$('.delete-item').click(function(e){
+$('ul').on('click','.delete-item', function(e){
   $(this).parent().parent().fadeOut(300, function(){
     $(this).remove();
   });
   e.stopPropagation();
+});
+
+
+
+
+$('input[type="text"]').keypress(function(e){
+  if(e.which === 13){
+    // Grabbing the text from input and hiting "Enter" button
+    var todoText = ($(this).val());
+    // Creating new 'li' and adding it to 'ul'
+    var newLi = '<li class="todo__list-item">'+ 
+    '<span class="todo__list-item-text">'+ todoText +'</span>'
+    +
+    '<span class="todo__list-item-edit"><span class="delete-item"> <img src="assets/css/sprites/garbage.svg" alt="delete item"></span><span class="check-item"> <img src="assets/css/sprites/checked.svg" alt="checked item"></span></span><span class="todo__list-item-underline"></span></li>'
+    $('ul').append(newLi);
+    clearInputField();
+  }
+});
+
+// Function to clear Input Field
+function clearInputField(){
+  $('input[type="text"]').val('');
+}
+
+$('#pulse2').click(function(){
+  $('input[type="text"]').toggle().focus();
 });
